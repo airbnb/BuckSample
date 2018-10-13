188d6ca94e17c938fb97e57a841f1e9e1221acf7
Steven Hepting <steven.hepting@airbnb.com>
2018-10-12 17:10:31 -0700

    Add new BuckSample

---
+++ BuckSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuckSample
+//
+//  Created by steven_hepting on 10/12/18.
+//  Copyright © 2018 Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ BuckSample/main.m
@@ -0,0 +1,14 @@
+
+
+#import <UIKit/UIKit.h>
+
+#import "BuckSample.h"
+
+/*! @brief Application main entry point.
+    @return Exit code.
+ */
+int main(int argc, char *argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
