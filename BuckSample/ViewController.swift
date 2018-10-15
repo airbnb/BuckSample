7f8be8396326c447715cf760d9291d819a799ba7
Steven Hepting <steven.hepting@airbnb.com>
2018-10-15 13:22:39 -0700

    Make background blue (when it works)

--- BuckSample/ViewController.swift
+++ BuckSample/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+
+    let view = UIView()
+    view.backgroundColor = .blue
+    self.view = view
+    
   }
 
 
