2d485831bbd24473255648e83259f313f2915dc1
Steven Hepting <steven.hepting@airbnb.com>
2018-12-12 14:46:21 -0800

    User boring libraries.

--- App/ViewController.swift
+++ App/ViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
 
-import ImportObjC
-import ImportFromAnotherLibrary
+//import Objc1
+import ImportSwift
+import Swift2
 
 public let testVar = "SampleValue"
 
@@ -27,13 +28,20 @@ class ViewController: UIViewController {
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
 
-    // Swift class defined in ImportObjC
-    AnotherCow.say("Jim")
+    // Swift class defined in ImportSwift
+    let c = Counter()
+    print("Test: %s", c)
 
-    // Swift class defined in ImportFromAnotherLibrary
-    Cow.say(name: "Bobby")
+    // Swift class defined in Swift2
+    let s = SwiftClass2()
+    print("Swift2: %s", s)
 
-    // Objective-C class defined in ImportFromAnotherLibrary
-    objC.callSwift(byName: "Tom")
+    // Objective C class defined in Objc1
+    // When this is uncommented, LLDB fails to print objects properly
+    // with the following error:
+    //   failed to get module 'ExampleAppLibrary' from AST context:
+    //   error: missing required module 'Objc1'
+//    let o = Objc1()
+//    print("Objc1: %s", o)
   }
 }
