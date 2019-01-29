fe859ff2da9f1f8d4ff082f2768479c0b1e9a28c
Dan Federman <dan.federman@airbnb.com>
2019-01-29 14:57:41 -0800

    Add unit test targets to two libraries

---
+++ Libraries/Swift3/Tests/SomeMoreSwift3Tests.swift
@@ -0,0 +1,13 @@
+import XCTest
+
+final class SomeMoreSwift3Tests: XCTestCase {
+
+  func test_nothingAtAll() {
+    XCTAssertTrue(true)
+  }
+
+  func test_false() {
+    XCTAssertFalse(false)
+  }
+
+}
---
+++ Libraries/Swift3/Tests/SomeSwift3Tests.swift
@@ -0,0 +1,13 @@
+import XCTest
+
+final class SomeSwift3Tests: XCTestCase {
+
+  func test_nothingAtAll() {
+    XCTAssertTrue(true)
+  }
+
+  func test_equal() {
+    XCTAssertEqual("", "")
+  }
+
+}
