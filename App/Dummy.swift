2a42c25e765e4b7eac1d346438d2d2b6a819fb42
Dan Federman <dan.federman@airbnb.com>
2019-02-03 11:19:06 -0800

    Review feedback

--- App/Dummy.swift
+++
@@ -1,2 +0,0 @@
-// This file is meant to be included in an apple_binary that otherwise wouldn't have any code in it.
-// Buck apple_binary declarations need at least one source file in them to be properly generated.
