0a5eb945778f8b77fea66b4d94116956929e0a79
Dan Federman <dan.federman@airbnb.com>
2019-03-18 13:53:25 -0700

    Get generated project working

--- Libraries/SwiftShared/Sources/PublicSharedClass.swift
+++ Libraries/SwiftShared/Sources/PublicSharedClass.swift
@@ -10,7 +10,7 @@ public class PublicSharedClass {
   }
 
   public func readFromResource() -> String {
-    if let filepath = Bundle(identifier: "com.airbnb.SwiftSharedFramework")?.path(forResource: "plaintext", ofType: "txt"),
+    if let filepath = Bundle(for: type(of: self)).path(forResource: "plaintext", ofType: "txt"),
        let content = try? String(contentsOfFile: filepath) {
       return content
     } else {
