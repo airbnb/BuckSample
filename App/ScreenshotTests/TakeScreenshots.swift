5f91e2528b18c2dbcd0183c62bcb61fc1a4abd5f
Michael Bachand <bachand.michael@gmail.com>
2019-03-22 00:25:25 -0700

    Create VCs in test

--- App/ScreenshotTests/TakeScreenshots.swift
+++ App/ScreenshotTests/TakeScreenshots.swift
@@ -9,15 +9,22 @@ class TakeScreenshots: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    func viewControllersToTest() -> [UIViewController] {
+      let backgroundColors: [UIColor] = [.red, .green, .blue]
+      return backgroundColors.map { color in
+        let viewController = ViewController() // From ExampleApp
+        let view = viewController.view
+        view.backgroundColor = .red
+        view.frame.size = CGSize(width: 1125, height: 2436)
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        return viewController
+      }
+    }
+
     func testTakeScreenshots() {
-      let viewController = ViewController() // From ExampleApp
-
-      let viewControllersToTest = [viewController]
-
       let screenshotter = Screenshotter()
-
-      let images = viewControllersToTest.map { screenshotter.imageFromViewController($0) }
-
-      images.map { print($0) /* save to disk or upload */ }
+      let images = viewControllersToTest().map { screenshotter.imageFromViewController($0) }
+      images.map { print("Image: \($0)" /* save to disk or upload */ }
     }
 }
