1c23e8c149cf8966f82840f3c7de72cff8ce9a36
Michael Bachand <michael.bachand@airbnb.com>
2019-03-22 17:00:49 -0700

    Create directory for storing images

--- App/ScreenshotTests/TakeScreenshots.swift
+++ App/ScreenshotTests/TakeScreenshots.swift
@@ -1,7 +1,9 @@
-import XCTest
 @testable import ExampleApp
+
+import Foundation
 import Screenshotter
 import UIKit
+import XCTest
 
 // MARK: - TakeScreenshots
 
@@ -10,31 +12,69 @@ final class TakeScreenshots: XCTestCase {
   // MARK: Internal
 
   override func setUp() {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    setUpImagesDirectory()
   }
 
-    func viewControllersToTest() -> [UIViewController] {
-      let backgroundColors: [UIColor] = [.red, .green, .blue]
-      return backgroundColors.map { color in
-        let viewController = ViewController() // From ExampleApp
-        viewController.view.backgroundColor = color
-        return viewController
-      }
+  func viewControllersToTest() -> [UIViewController] {
+    let backgroundColors: [UIColor] = [.red, .green, .blue]
+    return backgroundColors.map { color in
+      let viewController = ViewController() // From ExampleApp
+      viewController.view.backgroundColor = color
+      return viewController
     }
+  }
 
-    func testTakeScreenshots() {
-      let screenshotter = Screenshotter()
-      let images = viewControllersToTest().map { screenshotter.imageFromViewController($0) }
-      for image in images.compactMap({ $0 }) {
-        storeImage(image)
-      }
+  func testTakeScreenshots() {
+    let screenshotter = Screenshotter()
+    let images = viewControllersToTest().map { screenshotter.imageFromViewController($0) }
+    for image in images.compactMap({ $0 }) {
+      storeImage(image)
     }
   }
 
   // MARK: Private
 
+  private let fileManager = FileManager.default
+
+  private lazy var urlForImagesDirectory: URL = {
+    guard let documentsURL = FileManager.default.urls(
+      for: .documentDirectory,
+      in: .userDomainMask).first else
+    {
+      preconditionFailure("Cannot find Documents URL")
+    }
+
+    return documentsURL.appendingPathComponent("ScreenshotTests", isDirectory: true)
+  }()
+
+  /// Deletes the images directory if it exists. Creates the images directory.
+  private func setUpImagesDirectory() {
+    var isDirectory: ObjCBool = true
+    let directoryExists = fileManager.fileExists(
+      atPath:  urlForImagesDirectory.absoluteString,
+      isDirectory: &isDirectory)
+
+    if directoryExists {
+      do {
+        try fileManager.removeItem(at: urlForImagesDirectory)
+      }
+      catch {
+        fatalError("Unable to remove \(urlForImagesDirectory.absoluteString): \(error)")
+      }
+    }
+
+    do {
+      try fileManager.createDirectory(
+        at: urlForImagesDirectory,
+        withIntermediateDirectories: true,
+        attributes: nil)
+    }
+    catch {
+      fatalError("Unable to create \(urlForImagesDirectory.absoluteString): \(error)")
+    }
+  }
+
   private func storeImage(_ image: UIImage) {
-    // TODO(MB): implement
-    print("Storing image")
+    print(urlForImagesDirectory.absoluteString)
   }
 }
