0feab868c3292dc5d1d2b1912fd11a6922a50374
Dan Federman <dfed@me.com>
2019-04-07 00:35:52 -0700

    Remove unnecessary files and add better comments (#75)
    * Remove manually managed module map and umbrella header from repo
    
    * Better comments

--- Libraries/ObjcAndSwift/Sources/MyBridgedSwiftClassInMixedModule.swift
+++ Libraries/ObjcAndSwift/Sources/MyBridgedSwiftClassInMixedModule.swift
@@ -4,6 +4,7 @@ public class MyBridgedSwiftClassInMixedModule: NSObject {
 
   public func findObjectiveCInstance() -> Any {
     // Prove that Swift can find Objective C code in this module without a bridging header.
+    // Bridging headers aren't needed in library modules, since the umbrella header effectively acts as a bridging header.
     return MyBridgedObjcClassInMixedModule()
   }
 
