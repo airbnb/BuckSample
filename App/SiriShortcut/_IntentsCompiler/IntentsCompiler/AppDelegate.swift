676c06ba19a3f9d62e0cde38e0d99cae396a8867
Buck Sample <michael.bachand@airbnb.com>
2019-04-23 20:27:57 -0700

    Add IntentCompiler Xcode project

---
+++ App/SiriShortcut/_IntentsCompiler/IntentsCompiler/AppDelegate.swift
@@ -0,0 +1,17 @@
+// Created by Michael Bachand on 4/23/19.
+// Copyright © 2019 Airbnb Inc. All rights reserved.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+}
---
+++ App/SiriShortcut/_IntentsCompiler/README.md
@@ -0,0 +1,7 @@
+# IntentsCompiler Xcode Project
+
+`IntentDefinitionCodegen` is internal to Xcode. As such, Apple does not expose a standalone command for creating the Swift (or Objective-C) interface from an .intentdefinition.
+
+Airbnb has filed a Radar requesting this functionality.
+
+Until then, we are using this Xcode project to generate code from the .intentdefinition.
