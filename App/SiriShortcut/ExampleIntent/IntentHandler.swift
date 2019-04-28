41cbae1d773537b1b70c64239da42fccf56c7de9
Buck Sample <michael.bachand@airbnb.com>
2019-04-28 09:35:06 -0700

    Get intent building for CLI

--- App/SiriShortcut/ExampleIntent/IntentHandler.swift
+++ App/SiriShortcut/ExampleIntent/IntentHandler.swift
@@ -6,7 +6,7 @@ import Intents
 class IntentHandler: INExtension {
 
   override func handler(for intent: INIntent) -> Any {
-    if #available(iOSApplicationExtension 12.0, *) {
+    if #available(iOS 12.0, *) {
       guard intent is BuckPhotoIntent else {
         fatalError("Unhandled intent type: \(intent)")
       }
