ac938b3cc5f2aa9c0fbd4c8cdb963d46deaf6aa2
Buck Sample <michael.bachand@airbnb.com>
2019-04-28 21:19:58 -0700

    Remove MARK

--- App/SiriShortcut/ExampleIntentUI/IntentViewController.swift
+++ App/SiriShortcut/ExampleIntentUI/IntentViewController.swift
@@ -11,8 +11,6 @@ final class IntentViewController: UIViewController, INUIHostedViewControlling {
     // Do any additional setup after loading the view.
   }
 
-  // MARK: - INUIHostedViewControlling
-
   // Prepare your view controller for the interaction to handle.
   func configureView(for parameters: Set<INParameter>, of interaction: INInteraction, interactiveBehavior: INUIInteractiveBehavior, context: INUIHostedViewContext, completion: @escaping (Bool, Set<INParameter>, CGSize) -> Void) {
     // Do configuration here, including preparing views and calculating a desired size for presentation.
