380c70932f417ef916e9264e365033479d5353c2
Michael Bachand <michael.bachand@airbnb.com>
2019-05-09 01:00:35 -0700

    ONe more

--- App/SiriShortcut/ExampleIntent/IntentHandler.swift
+++ App/SiriShortcut/ExampleIntent/IntentHandler.swift
@@ -7,9 +7,11 @@ class IntentHandler: INExtension {
 
   override func handler(for intent: INIntent) -> Any {
     if #available(iOS 12.0, *) {
+      #if !DISABLE_SIRI_SHORTCUT
       guard intent is BuckPhotoIntent else {
         fatalError("Unhandled intent type: \(intent)")
       }
+      #endif
       return BuckPhotoIntentHandler()
     } else {
       fatalError("Unexpectly executing code path on iOS < 12.0")
