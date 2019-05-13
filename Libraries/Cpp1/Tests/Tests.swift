242cf0e4cb1b0abc7ec260766fdb4ecbe8f3a2d6
Maksym Grebenets <maksym.grebenets@cba.com.au>
2019-05-13 12:16:43 +1000

    Add unit tests for Cpp1 library
    Rename add functions for better use in Swift

--- Libraries/Cpp1/Tests/Tests.swift
+++ Libraries/Cpp1/Tests/Tests.swift
@@ -1,1 +1,11 @@
-// Your tests go here. Do not delete this file.
+import XCTest
+@testable import Cpp1
+
+final class Cpp1Tests: XCTestCase {
+    func testCpp1() {
+        XCTAssertEqual(Cpp1.add(1, 2), 3)
+
+        let cppObj = Cpp1()
+        XCTAssertEqual(cppObj.add(1, 2), 3)
+    }
+}
